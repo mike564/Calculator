a5a6452a16448155095da2028add1ef9073733a8
mike564 <mhnguyen555@outlook.com>
2022-03-03 20:47:09 -0600

    Add back button and add overflow handling

--- js/main.js
+++ js/main.js
@@ -4,95 +4,144 @@ function setMathState(e){
     if(storedNum!=null && textClearState==false){
         operate();
     }
-    storedNum = textBox.textContent;
+    storedNum = actualNum;
     textClearState = true;
     mathState=e.target.classList[1];
 }
 
 function operate(){
     let num1=Number(storedNum);
-    let num2=Number(textBox.textContent);
-
-    if(mathState=="add"){
-        textBox.textContent=(Math.round(add(num1,num2)*1000)/1000);
+    let num2=Number(actualNum);
+    let result=null;
+    if(mathState=="add"  && storedNum != null){
+        result=(Math.round(add(num1,num2)*1000)/1000);
     }
 
-    else if(mathState=="sub"){
-        textBox.textContent=(Math.round(subtract(num1,num2)*1000)/1000);
+    else if(mathState=="sub" && storedNum != null){
+        result=(Math.round(subtract(num1,num2)*1000)/1000);
     }
 
-    else if(mathState=="mult"){
-        textBox.textContent=(Math.round(multiply([num1,num2])*1000)/1000);
+    else if(mathState=="mult" && storedNum != null){
+        result=(Math.round(multiply([num1,num2])*1000)/1000);
     }
 
-    else if(mathState=="div"){
+    else if(mathState=="div" && storedNum != null){
         if(num2==0){
             textBox.textContent="Cannot divide by 0";
             textClearState = true;
             mathState = null;
             return;
         }
-        textBox.textContent=(Math.round(multiply([num1,1/num2])*1000)/1000);
+        result=(Math.round(multiply([num1,1/num2])*1000)/1000);
     }
-    storedNum = textBox.textContent;
-    mathState = null;
+
+    if(result!=null){
+        actualNum=String(result);
+        if(actualNum.length>15){
+            textBox.textContent = Number(result).toExponential(3);
+        }
+        else{
+            textBox.textContent = actualNum;
+        }
+
+        storedNum = actualNum;
+        mathState = null;
+    }
+    
+    textClearState = true;
 }
 
 function inputNumber(e){
     if(textClearState==true || textBox.textContent=="0"){
         textBox.textContent="";
+        actualNum="";
         textClearState=false;
     }
-    
-    if (textBox.textContent.length>15){
-        alert("Overflow");
-        /* textBox.textContent = Number(textBox.textContent+e.target.textContent).toExponential(); */
-
+    actualNum = actualNum+e.target.textContent;
+    if (actualNum.length>15 || textBox.textContent.indexOf('e') != -1){
+        textBox.textContent = String(Number(actualNum).toExponential(3));
     }
     else{
-        textBox.textContent = textBox.textContent+e.target.textContent;
-        
-    } 
+        textBox.textContent = actualNum;      
+    }
 }
 
 function inputDecimal(e){
-    if(textBox.textContent.indexOf('.') != -1){
+    if(textClearState==true || actualNum=="0"){
+        textBox.textContent="";
+        actualNum="";
+        textClearState=false;
+    }
+
+    if(actualNum.indexOf('.') != -1){
         return;
     }
-    if (textBox.textContent.length>15){
-        alert("Overflow");
+    actualNum = actualNum+e.target.textContent;
+    if (actualNum.length>15 || actualNum.indexOf('e') != -1){
+        textBox.textContent = String(Number(actualNum).toExponential(3));
     }
     else{
-        textBox.textContent = textBox.textContent+'.';
-        decimalState=true; 
-    } 
+        textBox.textContent = actualNum;
+    }
 }
 
 function makeNegative(){
-    if (textBox.textContent[0]=='-'){
-        textBox.textContent = textBox.textContent.slice(1,textBox.textContent.length);
+    if (actualNum[0]=='-'){
+        actualNum = actualNum.slice(1,actualNum.length);
     }
-    else{
-        textBox.textContent = '-' + textBox.textContent;
+    else if(actualNum!=0){
+        actualNum = '-' + actualNum;  
     }
+    textBox.textContent=actualNum;
 }
 
 function clear(){
-    textBox.textContent = "0";
+    actualNum = "0";
+    textBox.textContent = actualNum;
     storedNum = null;
     mathState = null;
 }
 
+function backSpace(){
+    if(actualNum.indexOf('e')!=-1){
+        textBox.textContent ="Too large to back";
+        textClearState=true;
+    }
+    else{
+        if(actualNum.length==1){
+            actualNum = "0";
+            textBox.textContent=actualNum;
+            return;
+        }
+        else{
+            actualNum = String(actualNum.slice(0,actualNum.length-1))
+            if (textBox.textContent.indexOf('e') != -1){
+                if(actualNum.length<16){
+                    textBox.textContent = actualNum;
+                }
+                else{
+                    textBox.textContent = String(Number(actualNum).toExponential(3));
+                }
+            }
+            else{
+                textBox.textContent = String(textBox.textContent).slice(0,textBox.textContent.length-1);
+            }
+        }
+        textClearState=false;
+    }   
+}
+
 let textClearState = true;
+let actualNum = "0";
 let storedNum = null;
 let mathState = null;
-let decimalState = false;
 
 const numPadBtns = Array.from(document.querySelectorAll('.number'));
 const operatorBtns = Array.from(document.querySelectorAll('.operator'));
 const textBox = document.querySelector('.text-box');
 const equalBtn = document.querySelector('.equal');
 const clearBtn = document.querySelector('.clear');
+const backBtn = document.querySelector('.back'); 
 const negativeBtn = document.querySelector('.negative');
 const decimalBtn = document.querySelector('.decimal');
 
@@ -100,5 +149,7 @@ numPadBtns.forEach(function(thisObj){thisObj.addEventListener('click',inputNumbe
 operatorBtns.forEach(function(thisObj){thisObj.addEventListener('click',setMathState)});
 equalBtn.addEventListener('click',operate);
 clearBtn.addEventListener('click',clear);
+backBtn.addEventListener('click',backSpace)
 negativeBtn.addEventListener('click',makeNegative);
 decimalBtn.addEventListener('click',inputDecimal);
+
